1115f87f541ffc1d73494bc74471b76ed2719bc8
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2025-05-22 11:30:03 +0500

    removed endpoints

--- main.go
+++ main.go
@@ -20,45 +20,10 @@ func main() {
 
 	// REST endpoints
 	r.GET("/hello", helloWorld)
-	r.GET("/users", getUsers)
-	r.GET("/users/:id", getUserByID)
-	r.POST("/users", createUser)
+	// r.GET("/users", getUsers)
+	// r.GET("/users/:id", getUserByID)
+	// r.POST("/users", createUser)
 
 	// Start the server on port 8080
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
-}
-
-// Function for hello world endpoint
-func helloWorld(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
-}
-
-// Function to get all users
-func getUsers(c *gin.Context) {
-	c.JSON(http.StatusOK, users)
-}
-
-// Function to get a user by ID
-func getUserByID(c *gin.Context) {
-	id := c.Param("id")
-	for _, user := range users {
-		if user.ID == id {
-			c.JSON(http.StatusOK, user)
-			return
-		}
-	}
-	c.JSON(http.StatusNotFound, gin.H{"message": "user not found"})
-}
-
-// Function to create a new user
-func createUser(c *gin.Context) {
-	var user User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	users = append(users, user)
-	c.JSON(http.StatusCreated, user)
 }
